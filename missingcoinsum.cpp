08a2d934cb1227ddb67447eb9a01765b755ea35f
Shubham Kapoor <shubham.kapoor01@gmail.com>
2022-01-17 12:36:54 +0400

    stick lengths added

---
+++ missingcoinsum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pb push_back
+#define pf push_front
+#define ff first
+#define ss second
+#define endl "\n"
+
+
+int main () {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	
+	return 0;
+}

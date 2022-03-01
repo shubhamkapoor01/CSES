ca479b4cceeb5c26c55d8f502189cb3c58393876
Shubham Kapoor <shubham.kapoor01@gmail.com>
2022-03-01 22:46:53 +0530

    roundtrip

--- roundtrip.cpp
+++ roundtrip.cpp
@@ -10,6 +10,28 @@ using namespace std;
 #define endl "\n"
 
 
+vector<ll> cycle;
+ll root;
+
+bool dfs(vector<vector<ll>>& graph, ll start, ll par,
+					vector<bool>& vis, vector<ll>& cyc) {
+
+	if (vis[start]) return false;
+	vis[start] = true;
+	cyc.push_back(start);
+
+	for (auto i: graph[start]) {
+		if ((vis[i] && par != i) || dfs(graph, i, start, vis, cyc)) {
+			cycle = cyc;
+			root = start;
+			return true;
+		}
+	}
+
+	cyc.pop_back();
+	return false;
+}
+
 int main () {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -25,6 +47,32 @@ int main () {
 		graph[v].push_back(u);
 	}
 
-	cout << endl;
+	vector<bool> vis(n + 1, false);
+	for (ll i = 1; i <= n; i ++) {
+		if (!vis[i]) {
+			vector<ll> cyc = {};
+			if (dfs(graph, i, -1, vis, cyc)) {
+				cycle.push_back(root);
+				ll idx = cycle.size() - 1;
+				ll cnt = 0;
+				while (true) {
+					if (cycle[idx] == root) {
+						cnt ++;
+					}
+					if (cnt == 2) {
+						break;
+					}
+					idx --;
+				}
+				cout << cycle.size() - idx + 1 << endl;
+				for (ll i = idx; i < cycle.size(); i ++) {
+					cout << cycle[i] << " ";
+				}
+				return 0;
+			}
+		}
+	}
+
+	cout << "IMPOSSIBLE";
 	return 0;
 }
